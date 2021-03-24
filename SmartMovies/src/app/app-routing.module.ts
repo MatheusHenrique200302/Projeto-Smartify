692f7bca12cbb2f1e4a1bb449fbd222967794073
MatheusHenrique200302 <36648821+MatheusHenrique200302@users.noreply.github.com>
2021-03-24 20:02:33 -0300

    added details option in route

--- SmartMovies/src/app/app-routing.module.ts
+++ SmartMovies/src/app/app-routing.module.ts
@@ -1,28 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetalhesComponent } from './detalhes/detalhes.component';
 import { FilmesComponent } from './filmes/filmes.component';
 import { SeriesComponent } from './series/series.component';
 
 const routes: Routes = [
-{
-  path:'',
-  redirectTo: "filmes",
-  pathMatch: 'full'
-  
-},
-{
-  path: 'filmes',
-  component: FilmesComponent
-},
-{
-  path: 'series',
-  component: SeriesComponent
-}
-
+  {
+    path: '',
+    redirectTo: 'filmes',
+    pathMatch: 'full',
+  },
+  {
+    path: 'filmes',
+    component: FilmesComponent,
+    children: [
+      {
+        path: '/detalhes/:id',
+        component: DetalhesComponent,
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: 'series',
+    component: SeriesComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

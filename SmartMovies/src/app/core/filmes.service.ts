5ae6f869a652025cd3e4af0783d939ee608d6f47
MatheusHenrique200302 <36648821+MatheusHenrique200302@users.noreply.github.com>
2021-03-24 14:28:55 -0300

    added movies listing

--- SmartMovies/src/app/core/filmes.service.ts
+++ SmartMovies/src/app/core/filmes.service.ts
@@ -1,23 +1,19 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { Filme } from '../shared/models/filmes';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
-
 export class FilmesService {
- url = 'localhost:3000/filmes';
+  constructor(private htpp: HttpClient) {}
 
-  constructor(private htpp : HttpClient) { }
+  url = 'localhost:3000/filmes';
 
-  getFilmes(page : number){
+  getFilmes(pagina: number): Observable<Filme[]> {
     let httpParams = new HttpParams();
-    httpParams.set('page',page.toString());
-    return this.htpp.get(this.url);
+    httpParams.set('page', pagina.toString());
+    return this.htpp.get<Filme[]>(this.url);
   }
-
-
-
 }
--- SmartMovies/src/app/filmes/filmes.component.ts
+++ SmartMovies/src/app/filmes/filmes.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FilmesService } from '../core/filmes.service';
+import { Filme } from '../shared/models/filmes';
 
 @Component({
   selector: 'app-filmes',
   templateUrl: './filmes.component.html',
-  styleUrls: ['./filmes.component.css']
+  styleUrls: ['./filmes.component.css'],
 })
 export class FilmesComponent implements OnInit {
+  constructor(private FilmeService: FilmesService) {}
 
-  constructor() { }
+  Filmes: Filme[] = [];
+  pagina: number = 0;
 
   ngOnInit(): void {
+    this.ListarFilmes();
   }
 
+  private ListarFilmes(): void {
+    this.pagina++;
+    this.FilmeService.getFilmes(this.pagina).subscribe((filmes: Filme[]) =>
+      this.Filmes.push(...filmes)
+    );
+    console.log(this.Filmes);
+  }
 }

655582dd7f41c424e8e7362388adb9fcc83536ba
MatheusHenrique200302 <36648821+MatheusHenrique200302@users.noreply.github.com>
2021-03-24 14:05:25 -0300

    created series interface

--- SmartMovies/src/app/shared/models/series.ts
+++ SmartMovies/src/app/shared/models/series.ts
@@ -1,9 +1,14 @@
 export interface Serie {
-    titulo?: string;
-    descricao?: string;
-    btnSucesso?: string ;
-    btnCancelar?: string ;
-    corBtnSucesso?: string;
-    corBtnCancelar?: string;
-    possuiBtnFechar?: boolean ;
+ backdrop_path: String,
+ first_air_date: Date,
+ genre_ids: Number[],
+ name: String,
+ origin_country: String[],
+ original_language: String,
+ original_name:String,
+ overview: String,
+ popularity: Number,
+ poster_path: String,
+ vote_average: Number,
+ vote_count: Number
 }

a6f4a5d0f2132b7bebd0dc326f5bdea19e51cda4
MatheusHenrique200302 <36648821+MatheusHenrique200302@users.noreply.github.com>
2021-03-24 14:06:12 -0300

    created http function to get movies

--- SmartMovies/src/app/core/filmes.service.ts
+++ SmartMovies/src/app/core/filmes.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class FilmesService {
 
-  constructor() { }
+
+export class FilmesService {
+ url = 'localhost:3000/filmes';
+
+  constructor(private htpp : HttpClient) { }
+
+  getFilmes(page : number){
+    let httpParams = new HttpParams();
+    httpParams.set('page',page.toString());
+    return this.htpp.get(this.url);
+  }
+
+
+
 }
